10cba6c5e6bc95050fdfeb79dd8e10986152a7f2
CrisAlvar3z <cris.alvarez.tobal@gmail.com>
2021-11-30 17:22:56 -0300

    cambios index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
         resave: false,
         saveUninitialized: true,
         cookie: {
-            secure: true,
+            secure: false,
             httpOnly: true,
             sameSite: 'None',
         }
