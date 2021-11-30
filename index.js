f14dcbb41715413dc68b53fe1bc237074fbb12ae
CrisAlvar3z <cris.alvarez.tobal@gmail.com>
2021-11-30 00:00:43 -0300

    algunos cambios

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/arriendos', require('./arriendos/arriendos.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 8080) : 8080;
 app.listen(port, () => console.log('Server listening on port ' + port));
 
 
