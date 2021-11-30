f86d3ca7018c9cd2a3b350178221e8645e5c96a3
CrisAlvar3z <cris.alvarez.tobal@gmail.com>
2021-11-30 16:37:55 -0300

    cambios en acount.controller.js

--- accounts/accounts.controller.js
+++ accounts/accounts.controller.js
@@ -240,8 +240,7 @@ function setTokenCookie(res, token) {
     // create cookie with refresh token that expires in 7 days
     const cookieOptions = {
         httpOnly: true,
-        expires: new Date(Date.now() + 7*24*60*60*1000),
-        secure : true
+        expires: new Date(Date.now() + 7*24*60*60*1000)
     };
     res.cookie('refreshToken', token, cookieOptions);
 }
